5af51c21052cd6977542a8ad3ca6214cff8e310f
Sigurd <j.skadborg00@gmail.com>
2023-10-06 11:04:21 +0200

    get all feeds async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dr_filter_engine::rssfeed::get_feed;
+use futures::future::join_all;
 
 #[tokio::main]
 async fn main() {
@@ -6,6 +7,34 @@ async fn main() {
 }
 
 async fn run() {
-    let content = get_feed("https://www.dr.dk/nyheder/service/feeds/senestenyt").await.unwrap();
-    println!("{}", content.title);
+    let feeds:Vec<&str> = vec![
+        "https://www.dr.dk/nyheder/service/feeds/senestenyt",
+        "https://www.dr.dk/nyheder/service/feeds/indland",
+        "https://www.dr.dk/nyheder/service/feeds/udland",
+        "https://www.dr.dk/nyheder/service/feeds/penge",
+        "https://www.dr.dk/nyheder/service/feeds/politik",
+        "https://www.dr.dk/nyheder/service/feeds/sporten",
+        "https://www.dr.dk/nyheder/service/feeds/senestesport",
+        "https://www.dr.dk/nyheder/service/feeds/viden",
+        "https://www.dr.dk/nyheder/service/feeds/kultur",
+        "https://www.dr.dk/nyheder/service/feeds/musik",
+        "https://www.dr.dk/nyheder/service/feeds/mitliv",
+        "https://www.dr.dk/nyheder/service/feeds/mad",
+        "https://www.dr.dk/nyheder/service/feeds/vejret",
+        "https://www.dr.dk/nyheder/service/feeds/regionale",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/kbh",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/bornholm",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/syd",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/fyn",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/vest",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/nord",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/trekanten",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/sjaelland",
+        "https://www.dr.dk/nyheder/service/feeds/regionale/oestjylland"
+    ];
+    let mut contents = vec![];
+    for feed in feeds {
+        contents.push(get_feed(feed));
+    }
+    let contents = join_all(contents).await;
 }
